ab9ece4e4216bd3bad67d461ac538373be29ecef
abhay-ch17 <abhaychouksey17@gmail.com>
2026-08-15 21:25:43 +0530

    add origin in cors

--- job_tracker_back/app.js
+++ job_tracker_back/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://jobb-tracker.netlify.app', //add frontend origin
     credentials:true
 }))
 
